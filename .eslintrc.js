2e00fac73fe385b77967aed858612e5b5ead955d
bootstet <chenyouzeng@chenyouzengdeMacBook-Air.local>
2020-10-23 17:42:24 +0800

    封装请求模块

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  // 自定义校验规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'semi': ['error', 'always']
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true
+      }
+    }]
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,34 @@
     <h1>App</h1>
     <!-- 根基路由出口 -->
     <router-view/>
+    <p class="text">hello </p>
+
   </div>
 </template>
+<script lang='ts'>
+import Vue from 'vue'
+import request from '@/utils/request'
 
-<style lang="scss" scoped></style>
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
+
+interface Foo {
+  a: number
+  b: string
+}
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped>
+  // @import "~@/styles/variables.scss";
+  .text {
+    color: $warning-color;
+  }
+</style>
